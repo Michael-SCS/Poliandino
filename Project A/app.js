99cf60132f45928a986a996e6c1c9f805fa44588
Michael Steven Castillo <94994717+Michael-SCS@users.noreply.github.com>
2021-12-19 12:18:00 -0500

    Add files via upload

---
+++ Project A/app.js
@@ -0,0 +1,32 @@
+/* Scroll */ 
+     window.sr = ScrollReveal();
+    sr.reveal('.containertexto', {
+        duration: 3000,
+        opacity: 0
+    }); 
+
+    window.sr = ScrollReveal();
+    sr.reveal('.containerimagen', {
+        duration: 3000,
+        opacity: 0
+    }); 
+
+    window.sr = ScrollReveal();
+    sr.reveal('.containerimagen2', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-400px'
+        
+    });  
+    window.sr = ScrollReveal();
+    sr.reveal('.containertexto2', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '-400px'
+    });  
+    window.sr = ScrollReveal();
+    sr.reveal('.card', {
+        duration: 2000,
+        origin: 'top',
+        distance: '-4000px'
+    });  
